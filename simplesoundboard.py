0399756cc37e258822cac36c23389ba18536a4a0
nickdanger3d <nick+github@bonfatti.net>
2013-10-22 17:04:11 -0400

    made it work!

--- simplesoundboard.py
+++ simplesoundboard.py
@@ -1,37 +1,76 @@
 import pygame
 import glob
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 pygame.mixer.init()
 
-filenames = glob.glob('sounds/*.wav')
-print filenames
-filenames.extend(glob.glob('sounds/*.mp3'))
-filenames=[os.path.basename(a) for a in filenames]
-print filenames
-page=""
-for name in filenames:
-	page= page + "<a href='/play/%s'>%s</a><br>"% (name,name)
-print page
+"""
+from MediaInfoDLL import MediaInfo, Stream
+from pydub import AudioSegment
+import shutil
 
+MI = MediaInfo()
+
+def fixSounds():
+	filenamestofix = glob.glob('incoming/*.wav')
+	filenamestofix.extend(glob.glob('incoming/*.mp3'))
+	basenamestofix=[os.path.basename(a) for a in filenamestofix]
+	tocopy=[]
+	toconvert=[]
+	for wav in basenamestofix:
+		a=MI.Open(os.path.join('incoming',wav))
+		print wav
+		audioformat=MI.Get(Stream.Audio,0,'Format')
+		if audioformat =='PCM':
+			#copy to sounds folder
+			shutil.move(os.path.join('incoming',wav), os.path.join('sounds',wav))
+			tocopy.append(wav)
+		else:
+			#convert to PCM, then copy to sounds folder
+
+			#converted=AudioSegment.from_mp3(os.path.join('incoming',wav))
+			#converted.export(os.path.join('sounds',wav[:4]+".wav"),format="wav")
+			toconvert.append(wav)
+	print "Needs converted: %s" % toconvert
+	print "Copied without conversion: %s" % tocopy
+
+			
+
+"""
 
 @app.route("/")
 def index():
-	return render_template('soundboard.html', filenames=filenames)
+	return render_template('soundboard.html', filenamesandcategories=categoriesAndTheirFiles, jqueryurl=url_for('static', filename='js/jquery.min.js'))
 	
-@app.route("/play/<name>")
-def play(name):
-	name=os.path.join('sounds',name)
+@app.route("/play/<category>/<name>")
+def play(category,name):
+	name=os.path.join('sounds',category,name)
+	#print name
 	pygame.mixer.music.load(name)
 	pygame.mixer.music.play()
+	#sounds[name].play()
 	return "ok"
 
 
 
-
+ 
 if __name__ == '__main__':
+	#fixSounds()
+	filenames = glob.glob('sounds/*/*.wav')
+	filenames.extend(glob.glob('sounds/*/*.mp3'))
+	print filenames
+	
+
+	filenamesdictlist=[{'category':os.path.basename(os.path.dirname(a)), 'file':os.path.basename(a)} for a in filenames]
+	categories=set([a['category'] for a in filenamesdictlist])
+	categoriesAndTheirFiles=[{'category':x, 'files':[y['file'] for y in filenamesdictlist if y['category']==x]} for x in categories]
+	#basenames=[os.path.basename(a) for a in filenames]
+	print filenames
+
+
+	#sounds=dict(zip(map(os.path.basename,filenames),map(pygame.mixer.Sound,filenames)))
 	app.debug = True
 	app.run(host='0.0.0.0')
