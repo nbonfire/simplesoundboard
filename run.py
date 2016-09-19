132ceb0d305de140aac278b44a2fb893fbdc9fcc
johnz1 <zillioxj@gmail.com>
2016-09-19 10:10:49 -0400

    Update run.py

--- run.py
+++ run.py
@@ -2,11 +2,11 @@
 from app import app
 #from config import SERVER_NAME, SERVER_PORT
 # debug mode
-app.run(host='0.0.0.0', port=8888, debug = True)
+#app.run(host='0.0.0.0', port=80, debug = True)
 # gevent wsgi
-#from gevent.wsgi import WSGIServer
+from gevent.wsgi import WSGIServer
 
 '''
-http_server = WSGIServer(('127.0.0.1', 8888), app)
+http_server = WSGIServer(('john-htpc', 80), app)
 http_server.serve_forever()
 '''
